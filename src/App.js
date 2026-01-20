28209281243c53b00743d2ed7a9f79aa13774aa3
vishal vadlik <vishalvadlik@email.com>
2026-01-20 11:08:30 +0530

    Fix demo first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './components/admin_side/js/demo_vishal'
+import Greet from './components/admin_side/js/demo_vishal';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Greet />
     </div>
   );
 }
