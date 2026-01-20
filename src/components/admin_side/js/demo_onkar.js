b84ac80e3acab8e249b9b48854384c2ebf8963ad
OnkarGhodke <ghodkeonkar84@gmail.com>
2026-01-20 11:36:48 +0530

    first commit

---
+++ src/components/admin_side/js/demo_onkar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Om() {
+    return(
+        <h1>Wel come</h1>
+    );
+}
+export default Om;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Om from './components/admin_side/js/script';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Om />
   </React.StrictMode>
 );
 
