3c9c2f8d36ab5537a3ee77fe60f343abb8b8cf3f
Ppatole21 <prajaktapatole01@gmail.com>
2026-01-20 12:06:08 +0530

    hi

---
+++ src/components/user_side/js/Body.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Body()
+{
+    return (
+    <div>
+
+        <div>
+          <h3>Product</h3>
+          <p>From ₹20,000</p>
+        </div>
+    </div>);
+}
+
+export default Body;
