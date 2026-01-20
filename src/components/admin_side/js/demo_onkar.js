df6c63eae4de8900a1f8e027d9e2ed071ef372cb
OnkarGhodke <ghodkeonkar84@gmail.com>
2026-01-20 11:53:59 +0530

    a

--- src/components/admin_side/js/demo_onkar.js
+++ src/components/admin_side/js/demo_onkar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Om() {
     return(
-        <h1>Wel come</h1>
+        <h1>Wel</h1>
     );
 }
 export default Om;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
-import Om from './components/admin_side/js/script';
+import Om from './components/admin_side/js/demo_onkar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
