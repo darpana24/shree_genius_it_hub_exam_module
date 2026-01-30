b153085d3b698750169347a7e1e754f216926893
dhage15 <vaishnavidhage411@gmail.com>
2026-01-30 22:40:22 +0530

    Result and Marksheet is completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import StudentTable from "./components/user_side/StudentTable";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StudentTable/>
       </header>
     </div>
   );
